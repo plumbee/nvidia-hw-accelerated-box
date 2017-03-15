8642b72db2ef9588946a843e4f3b62310565c42a
Piergiorgio Niero <piergiorgio.niero@gmail.com>
2017-03-15 13:13:27 +0000

    upgrading nvidia-docker to v1.0.1 to support docker v17

--- root_setup.sh
+++ root_setup.sh
@@ -77,7 +77,8 @@ apt-get install -y docker-engine
 usermod -aG docker ubuntu # assuming "ubuntu" is the user name
 
 # Nvidia docker https://github.com/NVIDIA/nvidia-docker/wiki
-wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0/nvidia-docker_1.0.0-1_amd64.deb
+export NVIDIADOCKER_VERSION=1.0.1
+wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v${NVIDIADOCKER_VERSION}/nvidia-docker_${NVIDIADOCKER_VERSION}-1_amd64.deb
 dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
 # Test nvidia-smi
 # nvidia-docker run --rm nvidia/cuda nvidia-smi
